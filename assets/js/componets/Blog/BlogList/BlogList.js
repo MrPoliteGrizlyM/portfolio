eb0d9cb9d0316be92459c0f9ff9548eae896c673
Ilya Maxutov <lbekework@gmail.com>
2020-01-15 14:51:00 -0800

    fix(BlogList): fix blog show on empty

--- assets/js/componets/Blog/BlogList/BlogList.js
+++ assets/js/componets/Blog/BlogList/BlogList.js
@@ -3,8 +3,8 @@ import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import { Query } from 'react-apollo';
 import ProjectBox from "./ProjectBox";
-import SkeletonBox from "./SkeletonBox";
-import {AlertServerError} from "../../elements/Alerts";
+import CircularProgress from '@material-ui/core/CircularProgress';
+import {AlertServerError, NothingToShow} from "../../elements/Alerts";
 import {LIST_PROJECTS} from "../../services/queries";
 import BlogHeader from "../BlogHeader/BlogHeader";
 
@@ -31,20 +31,21 @@ class BlogList extends Component {
                     <Query query={LIST_PROJECTS}>
                         {({ loading, error, data }) => {
                             if (loading) return (
-                                <>
-                                    <SkeletonBox/>
-                                    <SkeletonBox/>
-                                    <SkeletonBox/>
-                                </>
+                                <div className={classes.loadingBlock}>
+                                    <CircularProgress color="inherit" />
+                                </div>
                             );
                             if (error) return <AlertServerError className={classes.alertBlock}/>;
 
-                            return data.projects_list.projects.map((el, key) => <ProjectBox id={el.id}
-                                                                                            title={el.title}
-                                                                                            isActive={el.isActive}
-                                                                                            content={`${el.content.substr(0, 450)}...`}
-                                                                                            image={el.image}
-                                                                                            key={key}/>)
+                            if (data.projects_list.projects.length !== 0) {
+                                return data.projects_list.projects.map((el, key) => <ProjectBox id={el.id}
+                                                                                                title={el.title}
+                                                                                                isActive={el.isActive}
+                                                                                                content={`${el.content.substr(0, 450)}...`}
+                                                                                                image={el.image}
+                                                                                                key={key}/>)
+                            }
+                            return <NothingToShow className={classes.alertBlock}/>
                         }}
                     </Query>
                 </Grid>
--- assets/js/componets/elements/Alerts.js
+++ assets/js/componets/elements/Alerts.js
@@ -13,12 +13,23 @@ const AlertServerError = (props) => {
     return (
         <Grid className={props.className} xs={8} item>
             <Alert severity="error">
-                <AlertTitle>Error</AlertTitle>
+                <AlertTitle>Error ༼ಢ_ಢ༽</AlertTitle>
                 Something went wrong!
             </Alert>
         </Grid>
     )
 };
 
+const NothingToShow = (props) => {
+    const classes = makeClasses();
+    return (
+        <Grid className={props.className} xs={8} item>
+            <Alert severity="warning">
+                Nothing to show ¯\_(ツ)_/¯
+            </Alert>
+        </Grid>
+    )
+};
 
-export { AlertServerError };
+
+export { AlertServerError, NothingToShow };
