d0a1b0de670a470a2bc5da37ae1d56a53928523a
Ilya Maxutov <lbekework@gmail.com>
2020-01-16 00:45:07 -0800

    feature(logo): added animation to logo

--- assets/js/componets/elements/Header.js
+++ assets/js/componets/elements/Header.js
@@ -3,6 +3,8 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import { AboutButton, ContactButton} from "./StyledElements";
 import Logo from '../../images/logo.svg';
+import { ReactSVG } from 'react-svg'
+import {Link} from "react-router-dom";
 
 const styles = theme => ({
     container: {
@@ -19,7 +21,18 @@ const styles = theme => ({
     },
     logo: {
         width: "190px",
-        padding: "10px"
+        padding: "10px",
+        marginLeft: "50px",
+        [theme.breakpoints.down('xs')] :{
+            marginLeft: "0"
+        },
+        "& :hover svg .text-title" : {
+            animation: "translateTitle 3s infinite alternate"
+        },
+        "& :hover svg .ico" : {
+            transformOrigin: "20% 51%",
+            animation: "rotateIco 3s infinite alternate"
+        }
     },
     buttons: {
         lineHeight: "95px",
@@ -38,7 +51,9 @@ class Header extends Component {
                 <Grid item xs={12} md={10}>
                         <Grid className={classes.container} container>
                             <Grid className={classes.item} item xs={12} sm={4}>
-                                <img src={Logo} alt="" className={classes.logo}/>
+                                <Link to={"/app"}>
+                                    <ReactSVG src={Logo} className={classes.logo}/>
+                                </Link>
                             </Grid>
                             <Grid item xs={3} lg={4}>
 
